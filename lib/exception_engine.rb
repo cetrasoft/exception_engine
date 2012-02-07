6eab4841c28ed546f81586f5a7209ac126434d1a
Carlos Ramirez III <carlos@cetrasoft.com>
2012-02-07 16:04:07 -0500

    Fix mongo connection bug

--- lib/exception_engine.rb
+++ lib/exception_engine.rb
@@ -5,11 +5,13 @@ require "exception_engine/exception_middleware"
 require "exception_engine/engine"
 
 # We are required to choose a database name
-Mongoid.configure do |config|
-  name = "exception_engine-#{Rails.env}"
-  host = "localhost"
-  config.master = Mongo::Connection.new.db(name)
-  config.persist_in_safe_mode = false
+if Mongoid.config.databases.empty?
+  Mongoid.configure do |config|
+    name = "exception_engine-#{Rails.env}"
+    host = "localhost"
+    config.master = Mongo::Connection.new.db(name)
+    config.persist_in_safe_mode = false
+  end
 end
 
 module ExceptionEngine
